f5c62ff7e0aa74420a45dc0f5acdb2f1cac9601e
Mitchell Dowd <mitchell.dowd@winning.com.au>
2020-03-13 09:59:26 +1100

    Upgraded to Angular 8.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import {
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', loadChildren: './modules/dashboard/dashboard.module#DashboardModule' },
-  { path: 'heroes', loadChildren: './modules/heroes/heroes.module#HeroesModule' },
-  { path: 'villains', loadChildren: './modules/villains/villains.module#VillainsModule' },
+  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'heroes', loadChildren: () => import('./modules/heroes/heroes.module').then(m => m.HeroesModule) },
+  { path: 'villains', loadChildren: () => import('./modules/villains/villains.module').then(m => m.VillainsModule) },
 ];
 
 @NgModule({
